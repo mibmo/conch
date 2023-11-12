fc0689984b5201857e7c78badfa4b17cfaa8ebd1
mib <mib@mib.dev>
2023-11-12 22:08:54 +0100

    feat(templates): basic latex template

---
+++ templates/latex/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    conch = {
+      url = "github:mibmo/conch";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { conch, ... }:
+    conch.load [
+      "aarch64-darwin"
+      "riscv64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ]
+      ({ pkgs, ... }: {
+        packages = with pkgs; [
+          texlive.combined.scheme-full
+          pandoc
+        ];
+      });
+}
