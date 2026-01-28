123480a5c72c8c5bda11c7ba4f888d471eaf5525
mib <mib@kanp.ai>
2026-01-28 16:34:05 +0100

    have rust template use updated packages

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -7,16 +7,36 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    conch-rust.url = "github:mibmo/conch-rust";
+    conch-rust = {
+      url = "github:mibmo/conch-rust";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        conch.follows = "conch";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{ conch, ... }:
-    conch.load {
-      imports = [
-        inputs.conch-rust.conchModules.rust
-      ];
+    conch.load (
+      { pkgs, ... }:
+      {
+        imports = [
+          inputs.conch-rust.conchModules.rust
+        ];
 
-      rust.enable = true;
-    };
+        rust.enable = true;
+
+        shell.packages = with pkgs; [
+          pkg-config
+          openssl
+        ];
+      }
+    );
 }
