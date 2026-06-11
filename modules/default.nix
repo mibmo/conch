72c4d4ab2e24142558dd09d1a66b3dd49bd67d28
mib <mib@kanp.ai>
2026-06-11 17:11:00 +0200

    allow passing sets straight through as values instead of generators

--- modules/default.nix
+++ modules/default.nix
@@ -14,7 +14,7 @@ let
     ;
   inherit (lib.options) mkOption;
   inherit (lib.strings) concatStringsSep escapeShellArg makeLibraryPath;
-
+  inherit (lib.trivial) isFunction;
 in
 {
   imports = [
@@ -30,73 +30,79 @@ in
     };
 
     packages = mkOption {
-      type = with lib.types; functionTo (attrsOf package);
-      default = _: { };
-      apply = conch.applySystemsWithGenerator conch.genericGenerator;
+      type = with lib.types; either (attrsOf (attrsOf package)) (functionTo (attrsOf package));
+      default = { };
+      apply = conch.attrsOrApplySystemsWithGenerator conch.genericGenerator;
     };
 
     devShells = mkOption {
-      type = with lib.types; attrsOf (functionTo (submodule ./shell.nix));
+      type =
+        with lib.types;
+        either (attrsOf (attrsOf package)) (attrsOf (functionTo (submodule ./shell.nix)));
       default = { };
-      apply = conch.applySystemsWithGenerator (
-        system:
-        let
-          args = conch.makeArgs system;
-          applyShell =
-            shell:
-            {
-              # @TODO: include formatter once it's done
-              packages = shell.packages; # ++ [ config.formatter ];
-              LD_LIBRARY_PATH = makeLibraryPath shell.libraries;
-              inputsFrom = attrValues config.packages.${system};
-              shellHook =
-                let
-                  aliasCmd = foldlAttrs (
-                    acc: name: value:
-                    acc + "alias ${escapeShellArg name}=${escapeShellArg value};"
-                  ) "" shell.aliases;
-                  envCmd = foldlAttrs (
-                    acc: name: value:
-                    acc + "export ${escapeShellArg name}=${escapeShellArg value};"
-                  ) "" shell.environment;
-                in
-                concatStringsSep "\n" (
-                  [
-                    aliasCmd
-                    envCmd
-                  ]
-                  ++ shell.hooks
-                  ++ [ shell.hook ]
-                );
-            }
-            // shell.extraOpts;
-        in
-        mapAttrs (
-          _: shell:
-          let
-            config = shell args;
-            mkShell = if config.mkShell != null then config.mkShell else args.pkgs.mkShell;
-          in
-          mkShell (applyShell config)
-        )
-      );
+      apply =
+        value:
+        if value == { } then
+          { }
+        else if isFunction value then
+          conch.applySystemsWithGenerator (
+            system:
+            let
+              args = conch.makeArgs system;
+              applyShell =
+                shell:
+                {
+                  # @TODO: include formatter once it's done
+                  packages = shell.packages; # ++ [ config.formatter ];
+                  LD_LIBRARY_PATH = makeLibraryPath shell.libraries;
+                  inputsFrom = attrValues config.packages.${system};
+                  shellHook =
+                    let
+                      aliasCmd = foldlAttrs (
+                        acc: name: value:
+                        acc + "alias ${escapeShellArg name}=${escapeShellArg value};"
+                      ) "" shell.aliases;
+                      envCmd = foldlAttrs (
+                        acc: name: value:
+                        acc + "export ${escapeShellArg name}=${escapeShellArg value};"
+                      ) "" shell.environment;
+                    in
+                    concatStringsSep "\n" (
+                      [
+                        aliasCmd
+                        envCmd
+                      ]
+                      ++ shell.hooks
+                      ++ [ shell.hook ]
+                    );
+                }
+                // shell.extraOpts;
+            in
+            mapAttrs (
+              _: shell:
+              let
+                config = shell args;
+                mkShell = if config.mkShell != null then config.mkShell else args.pkgs.mkShell;
+              in
+              mkShell (applyShell config)
+            )
+          ) value
+        else
+          value;
     };
 
     formatter = mkOption {
-      type = with lib.types; nullOr (functionTo package);
-      default = null;
-      apply =
-        formatters:
-        if formatters == null then
-          { }
-        else
-          conch.applySystemsWithGenerator conch.genericGenerator formatters;
+      type = with lib.types; either (attrsOf (attrsOf package)) (functionTo package);
+      default = { };
+      apply = conch.attrsOrApplySystemsWithGenerator conch.genericGenerator;
     };
   };
 
   config._module.args.conch = {
     applySystemsWithGenerator =
       maker: value: listToAttrs (map (system: nameValuePair system (maker system value)) config.systems);
+    attrsOrApplySystemsWithGenerator =
+      maker: value: if isFunction value then conch.applySystemsWithGenerator maker value else value;
     genericGenerator = system: maker: maker (conch.makeArgs system);
     makeArgs = system: {
       inherit system lib;
